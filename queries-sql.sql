17eb47f2a9f95d288fd73e0f56475725fb9f7a84
Kathleen Tran <kathleen1.tran@gmail.com>
2024-09-05 14:27:08 -0500

    found second wittness

--- queries-sql.sql
+++ queries-sql.sql
@@ -1,19 +1,21 @@
 -- A ​murder​ that occurred on ​Jan.15, 2018​ and that it took place in ​SQL City​.
-
-
 SELECT * FROM crime_scene_report
 WHERE city ='SQL City'
 AND type='murder'
 AND date='20180115'
 
--- The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".
+-- RESULTS: The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".
 
-Write a query that identifies the first witness
+-- Write a query that identifies the first witness
 SELECT * FROM person
 WHERE address_street_name='Northwestern Dr'
 ORDER BY address_number DESC LIMIT 1
 
--- 14887	Morty Schapiro	118009	4919	Northwestern Dr	111564949
+-- RESULT: 14887	Morty Schapiro	118009	4919	Northwestern Dr	111564949
 
-Write a query that identifies the second witness.
+-- Write a query that identifies the second witness.
+SELECT * FROM person
+WHERE address_street_name='Franklin Ave'
+AND name LIKE 'Annabel%'
 
+-- RESULT: 16371	Annabel Miller	490173	103	Franklin Ave	318771143
